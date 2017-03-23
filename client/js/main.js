4fea46a1401a26e21c0f5a20b0fad22cb8974c77
Phillip_Chan <phillip.chan@liferay.COM>
2017-03-23 11:03:41 -0700

    Finished chat app with json object sending

--- client/js/main.js
+++ client/js/main.js
@@ -3,11 +3,20 @@ var socket = io();
 $('.chat-input').on('keypress', function (e) {
 	var self = $(this);
      if(e.which === 13){
+     	var message = {};
 
         //Disable textbox to prevent multiple submit
-        var message = self.val();
+        message.userMessage = self.val();
         socket.emit('chat message', message);
 
         self.val("");
      }
 });
+
+socket.on('chat message', function(message){
+  $('.message-list').append($('<li>').text(message.userMessage));
+
+  var messageList = document.querySelector(".message-list");
+	messageList.scrollTop = messageList.scrollHeight;
+});
+
--- server/app.js
+++ server/app.js
@@ -19,7 +19,8 @@ io.on('connection', function(socket){
     console.log('user disconnected');
   });
 
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+  socket.on('chat message', function(message){
+  	console.log(message)
+    io.emit('chat message', message);
   });
 });
