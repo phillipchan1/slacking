68d08634aec957318534af486e6055c7224584e5
Phillip_Chan <phillip.chan@liferay.COM>
2017-03-23 10:02:54 -0700

    Site structure set up

---
+++ server/app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use('/', express.static('client/'));
